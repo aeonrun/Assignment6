9af633f26e87bc38e2d7cbba1e9f363bfeda84cd
U6025731 <joyceshen@u6025731-tpm-a.local.ten.thomsonreuters.com>
2015-08-11 00:51:28 -0400

    first page slide

---
+++ custom.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+
+	$(this).click(function(){
+
+	$("#topLeft").animate({
+	top:"-=500px"
+	});
+
+	$("#bottomLeft").animate({
+		bottom:"-=500px"
+	});
+
+	$("#topRight").animate({
+		top:"-=500px"
+	});
+
+	$("#bottomRight").animate({
+		bottom:"-=500px"
+	});
+
+	});
+
+
+});
